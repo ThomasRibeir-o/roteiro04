105a322126ad968a0f966bef482edba781ee83f9
THOMÁS RIBEIRO PINTO <14500391622@campusrp-lab.riopomba.ifsudestemg.edu.br>
2026-05-15 10:13:08 -0300

    first version

---
+++ src/main/java/com/mycompany/roteiro04/Roteiro04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.roteiro04;
+
+/**
+ *
+ * @author 14500391622
+ */
+public class Roteiro04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
